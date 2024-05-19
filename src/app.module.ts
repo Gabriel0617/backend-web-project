2bc4bd69bd54fed5cebc931064d1ae0a532a39f6
Gabriel0617 <gabriel.fdez0617@gmail.com>
2024-05-19 05:45:36 -0400

    Changed the location of the entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
-import { DriverModule } from './driver/driver.module';
+import { DriverModule } from './entities/driver/driver.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { CarModule } from './car/car.module';
-import { BusBrandController } from './bus_brand/bus_brand.controller';
-import { BusBrandService } from './bus_brand/bus_brand.service';
-import { BusBrandModule } from './bus_brand/bus_brand.module';
-import { UserModule } from './user/user.module';
-import { CustomerModule } from './customer/customer.module';
-import { RoadMapModule } from './road_map/road_map.module';
-import { ServiceModule } from './service/service.module';
-import { TouristGroupModule } from './tourist_group/tourist_group.module';
-import { ServicePerformedModule } from './service_performed/service_performed.module';
-import { RequestModule } from './request/request.module';
-import { RoleModule } from './role/role.module';
-import { RoleService } from './role/role.service';
-import { RoleController } from './role/role.controller';
+import { CarModule } from './entities/car/car.module';
+import { BusBrandController } from './entities/bus_brand/bus_brand.controller';
+import { BusBrandService } from './entities/bus_brand/bus_brand.service';
+import { BusBrandModule } from './entities/bus_brand/bus_brand.module';
+import { UserModule } from './entities/user/user.module';
+import { CustomerModule } from './entities/customer/customer.module';
+import { RoadMapModule } from './entities/road_map/road_map.module';
+import { ServiceModule } from './entities/service/service.module';
+import { TouristGroupModule } from './entities/tourist_group/tourist_group.module';
+import { ServicePerformedModule } from './entities/service_performed/service_performed.module';
+import { RequestModule } from './entities/request/request.module';
+import { RoleModule } from './entities/role/role.module';
+import { RoleService } from './entities/role/role.service';
+import { RoleController } from './entities/role/role.controller';
 
 @Module({
   imports: [ DriverModule, PrismaModule, CarModule, BusBrandModule, UserModule, RoleModule, CustomerModule, RoadMapModule, ServiceModule, TouristGroupModule, ServicePerformedModule, RequestModule],
